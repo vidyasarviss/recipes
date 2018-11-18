a4913f769bd55355ce14a0ce413f3e7813925fd0
vidyasarviss <44604630+vidyasarviss@users.noreply.github.com>
2018-11-18 15:24:53 +0530

    Create item_database.sql

---
+++ item_database.sql
@@ -0,0 +1,313 @@
+-- MySQL dump 10.15  Distrib 10.0.36-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: item_database
+-- ------------------------------------------------------
+-- Server version	10.0.36-MariaDB-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Vegetarian'),(3,'Non Vegetarian');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredients`
+--
+
+DROP TABLE IF EXISTS `ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ingredients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `recipe_id` int(11) NOT NULL,
+  `unit_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `recipe_id` (`recipe_id`),
+  KEY `unit_id` (`unit_id`),
+  KEY `item_id` (`item_id`),
+  CONSTRAINT `ingredients_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`),
+  CONSTRAINT `ingredients_ibfk_2` FOREIGN KEY (`unit_id`) REFERENCES `units` (`id`),
+  CONSTRAINT `ingredients_ibfk_3` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=119 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredients`
+--
+
+LOCK TABLES `ingredients` WRITE;
+/*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
+INSERT INTO `ingredients` VALUES (87,4,10,46,3),(102,3,2,59,1),(104,3,13,56,1),(105,4,45,56,3),(110,2,10,65,1),(111,3,1,66,4),(112,3,10,52,4),(118,16,1,66,4);
+/*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(250) NOT NULL,
+  `purchase_unit` int(11) NOT NULL,
+  `sell_unit` int(11) NOT NULL,
+  `usage_unit` int(11) NOT NULL,
+  `sell_unit_qty` int(11) NOT NULL,
+  `usage_unit_qty` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_purchase_unit_id` (`purchase_unit`),
+  KEY `fk_sell_unit_id` (`sell_unit`),
+  KEY `fk_usage_unit_id` (`usage_unit`),
+  CONSTRAINT `fk_purchase_unit_id` FOREIGN KEY (`purchase_unit`) REFERENCES `units` (`id`),
+  CONSTRAINT `fk_sell_unit_id` FOREIGN KEY (`sell_unit`) REFERENCES `units` (`id`),
+  CONSTRAINT `fk_usage_unit_id` FOREIGN KEY (`usage_unit`) REFERENCES `units` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (2,'item 2',3,1,7,24,750),(3,'sugar',1,1,4,23,577),(4,'water',1,7,3,20,250),(14,'oil',1,3,4,34,70),(16,'all purpose flour',1,4,6,3,2),(17,'sugar1',1,4,4,2,3),(18,'rice',1,1,4,0,0);
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase_order_items`
+--
+
+DROP TABLE IF EXISTS `purchase_order_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchase_order_items` (
+  `id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `unit_id` int(11) NOT NULL,
+  `purchase_order_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `warehouse_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`),
+  KEY `unit_id` (`unit_id`),
+  KEY `purchase_order_id` (`purchase_order_id`),
+  KEY `warehouse_id` (`warehouse_id`),
+  CONSTRAINT `purchase_order_items_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
+  CONSTRAINT `purchase_order_items_ibfk_2` FOREIGN KEY (`unit_id`) REFERENCES `units` (`id`),
+  CONSTRAINT `purchase_order_items_ibfk_3` FOREIGN KEY (`purchase_order_id`) REFERENCES `purchase_orders` (`id`),
+  CONSTRAINT `purchase_order_items_ibfk_4` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase_order_items`
+--
+
+LOCK TABLES `purchase_order_items` WRITE;
+/*!40000 ALTER TABLE `purchase_order_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchase_order_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase_orders`
+--
+
+DROP TABLE IF EXISTS `purchase_orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchase_orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supplier_id` int(11) DEFAULT NULL,
+  `transaction_date` date NOT NULL,
+  `required_date` date NOT NULL,
+  `type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_purchase_orders_1_idx` (`supplier_id`),
+  CONSTRAINT `fk_purchase_orders_1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase_orders`
+--
+
+LOCK TABLES `purchase_orders` WRITE;
+/*!40000 ALTER TABLE `purchase_orders` DISABLE KEYS */;
+INSERT INTO `purchase_orders` VALUES (7,2,'2018-11-18','2018-02-05',2),(8,2,'2018-11-18','2018-11-18',NULL),(12,1,'2018-11-18','2018-11-18',NULL),(16,1,'2018-11-18','2018-11-18',NULL),(17,1,'2020-02-02','2018-11-18',NULL),(18,2,'2018-11-18','2018-11-18',NULL),(19,2,'2018-11-18','2018-11-18',NULL),(20,2,'2018-11-18','2018-11-18',NULL),(21,2,'2018-11-18','2018-11-18',NULL),(22,1,'2018-11-18','2018-11-18',NULL),(23,1,'2018-11-18','2018-11-18',NULL),(24,1,'2018-11-18','2018-11-18',NULL),(25,1,'2018-11-18','2018-11-18',NULL),(26,1,'2018-11-18','2018-11-18',NULL),(27,1,'2018-11-18','2018-11-18',NULL),(28,1,'2018-11-18','2018-11-18',NULL),(29,1,'2018-11-18','2018-11-18',NULL),(30,1,'2018-11-18','2018-11-18',NULL),(31,1,'2018-11-18','2018-11-18',NULL),(32,1,'2018-11-18','2018-11-18',NULL),(33,1,'2018-11-18','2018-11-18',NULL),(34,1,'2018-11-18','2018-11-18',NULL),(35,1,'2018-11-18','2018-11-18',NULL),(36,1,'2018-11-18','2018-11-18',NULL),(37,1,'2018-11-18','2018-11-18',NULL),(38,1,'2018-11-18','2018-11-18',NULL),(39,1,'2018-11-18','2018-11-18',NULL),(40,1,'2018-11-18','2018-11-18',NULL),(41,1,'2018-11-18','2018-11-18',NULL),(42,1,'2018-11-18','2018-11-18',NULL),(43,2,'2018-11-18','2018-11-18',NULL),(44,2,'2018-11-18','2018-11-18',NULL),(45,2,'2018-11-18','2018-11-18',NULL),(46,2,'2018-11-18','2018-11-18',NULL),(47,2,'2018-11-18','2018-11-18',NULL),(48,2,'2018-11-18','2018-11-18',NULL),(49,2,'2018-11-18','2018-11-18',NULL),(50,2,'2018-11-18','2018-11-18',NULL),(51,2,'2018-11-18','2018-11-18',NULL),(52,2,'2018-11-18','2018-11-18',NULL),(53,2,'2018-11-18','2018-11-18',NULL);
+/*!40000 ALTER TABLE `purchase_orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipes`
+--
+
+DROP TABLE IF EXISTS `recipes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipes` (
+  `recipes_name` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `preparation_method` text NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipes`
+--
+
+LOCK TABLES `recipes` WRITE;
+/*!40000 ALTER TABLE `recipes` DISABLE KEYS */;
+INSERT INTO `recipes` VALUES ('lemon coffee','Vegetarian','Heat up a cup of water and Add 1 to 2teaspoons of instant coffee to mug mix in sugar if desired.nnnnnn',46),('coffee1','Vegetarian','sdvcsfv',50),('coffee2','Vegetarian','sdvcsfv',51),('black coffee','Vegetarian','grthttjuyyyyyyyyyyj',52),('sweet puri','Vegetarian','grtyht',53),('panipuri','Vegetarian','grtyht cdyjtu,,  yil',56),('lemon tea','Vegetarian','take  a 250 grms of floor  add a 25ml of water and a pinch of salt and make a dough of it.......... ',59),('rice','Non Vegetarian','tuytjuytg',65),('Dosa','Vegetarian','reytyuytnnnnnnnnnnnn',66);
+/*!40000 ALTER TABLE `recipes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock_transactions`
+--
+
+DROP TABLE IF EXISTS `stock_transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock_transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` varchar(45) NOT NULL,
+  `warehouse_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `transaction_date` date NOT NULL,
+  `transaction_time` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stock_transactions_ibfk_1` (`warehouse_id`),
+  CONSTRAINT `stock_transactions_ibfk_1` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock_transactions`
+--
+
+LOCK TABLES `stock_transactions` WRITE;
+/*!40000 ALTER TABLE `stock_transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stock_transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `suppliers`
+--
+
+DROP TABLE IF EXISTS `suppliers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suppliers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+LOCK TABLES `suppliers` WRITE;
+/*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
+INSERT INTO `suppliers` VALUES (1,'abcc'),(2,'baaaaaaaaaaaa'),(3,'aaaaaaaaaaa1');
+/*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `units`
+--
+
+DROP TABLE IF EXISTS `units`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `units` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `units`
+--
+
+LOCK TABLES `units` WRITE;
+/*!40000 ALTER TABLE `units` DISABLE KEYS */;
+INSERT INTO `units` VALUES (1,'Carton'),(2,'grams'),(3,'ml'),(4,'kg'),(6,'grams'),(7,'box'),(26,'carton1');
+/*!40000 ALTER TABLE `units` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `warehouses`
+--
+
+DROP TABLE IF EXISTS `warehouses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `warehouses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `warehouses`
+--
+
+LOCK TABLES `warehouses` WRITE;
+/*!40000 ALTER TABLE `warehouses` DISABLE KEYS */;
+INSERT INTO `warehouses` VALUES (2,'bbbbbbbbbbbbbbbb1'),(3,'nnnnnnnnnnnnnn1'),(4,'warehouse1');
+/*!40000 ALTER TABLE `warehouses` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-18 15:23:56
